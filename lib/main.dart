c3e84548cb4365490e32e52d73f723031ce7d893
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-03-30 08:17:37 +0200

    move routes to other file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,11 +12,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
-      },
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings) =>
+          MaterialPageRoute(builder: (BuildContext context) => AlertPage()),
     );
   }
 }
