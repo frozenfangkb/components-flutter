d46854ace240004c621066c6adde021da8359b50
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-04-14 18:05:35 +0200

    inputs

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  InputPage({Key key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre;
+  String _email;
+  String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Inputs de texto')),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        children: [
+          _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearPersona()
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      // autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          counter: Text('Letras ${_nombre.length}'),
+          hintText: 'Nombre de la persona',
+          labelText: 'Nombre',
+          helperText: 'Sólo es el nombre',
+          suffixIcon: Icon(Icons.accessibility),
+          icon: Icon(Icons.account_circle)),
+      onChanged: (value) {
+        setState(() {
+          _nombre = value;
+        });
+      },
+    );
+  }
+
+  _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email es: $_email'),
+    );
+  }
+
+  _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Email de la persona',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)),
+      onChanged: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
+
+  _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Password de la persona',
+          labelText: 'Password',
+          suffixIcon: Icon(Icons.lock),
+          icon: Icon(Icons.lock)),
+      onChanged: (value) {
+        setState(() {
+          _password = value;
+        });
+      },
+    );
+  }
+}
